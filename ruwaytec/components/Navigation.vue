e8089082a3b18d1de221cf1c554267a69e161237
Abraham CASO <acasot@uni.pe>
2022-05-19 23:39:13 -0500

    agregue la plantilla de la web ruway tec

---
+++ ruwaytec/components/Navigation.vue
@@ -0,0 +1,46 @@
+<template>
+    <ul class="main-menu nav justify-content-center">
+        <li>
+            <n-link to="/">Inicio</n-link>
+        </li>
+        <li>
+            <n-link to="/about">Nosotros</n-link>
+        </li>
+        <li class="has-submenu">
+            <n-link to="/service">Servicios</n-link>
+            <ul class="submenu-nav">
+                <li>
+                    <n-link to="/service">Servicios</n-link>
+                </li>
+                <li>
+                    <n-link to="/service-details">Detalles de servicios</n-link>
+                </li>
+            </ul>
+        </li>
+        <li class="has-submenu">
+            <n-link to="/project">Proyectos</n-link>
+            <ul class="submenu-nav">
+                <li>
+                    <n-link to="/project">Proyestos</n-link>
+                </li>
+                <li>
+                    <n-link to="/project-details">Detalles de Proyectos</n-link>
+                </li>
+            </ul>
+        </li>
+        <li class="has-submenu">
+            <n-link to="/blog">Blog</n-link>
+            <ul class="submenu-nav">
+                <li>
+                    <n-link to="/blog">Blog</n-link>
+                </li>
+                <li>
+                    <n-link to="/blog-details">Detalles de Blog</n-link>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <n-link to="/contact">Contactos</n-link>
+        </li>
+    </ul>
+</template>
